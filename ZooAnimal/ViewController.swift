a81654f19bbfa222f1104d8756c59f4a9e963725
Konstantin Fomenkov <kolij2005@gmail.com>
2022-04-12 22:46:12 +0300

    Initial Commit

---
+++ ZooAnimal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZooAnimal
+//
+//  Created by Konstantin Fomenkov on 12.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
