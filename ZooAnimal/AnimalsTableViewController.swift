0a6a122279c5ab5614ba95c3ebfa54f129c6dda4
Konstantin Fomenkov <kolij2005@gmail.com>
2022-04-13 22:08:04 +0300

    final without images in tableView

--- ZooAnimal/AnimalsTableViewController.swift
+++ ZooAnimal/AnimalsTableViewController.swift
@@ -15,11 +15,9 @@ class AnimalsTableViewController: UITableViewController {
         super.viewDidLoad()
         tableView.rowHeight = 60
         fetchAnimals()
-        tableView.reloadData()
     }
 
         // MARK: - Table view data source
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         animals.count
     }
@@ -31,28 +29,25 @@ class AnimalsTableViewController: UITableViewController {
         content.text = "Animal: \(animal.name)"
         content.secondaryText = "Latin name: \(animal.latinName)"
         NetworkManager.shared.fetchImage(from: animal.imageLink) { data in
-                content.image = UIImage(data: data)
-            }
+            content.image = UIImage(data: data)
+        }
         cell.contentConfiguration = content
-
         return cell
     }
 
+        // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let animalVC = segue.destination as? AnimalViewController else { return }
+        guard let indexPath = tableView.indexPathForSelectedRow else { return }
+        animalVC.animal = animals[indexPath.row]
+    }
 
-     // MARK: - Navigation
-
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-         guard let animalVC = segue.destination as? AnimalViewController else { return }
-         guard let indexPath = tableView.indexPathForSelectedRow else { return }
-         animalVC.animal = animals[indexPath.row]
-     }
-
+        // MARK: - Data function
     func fetchAnimals() {
         NetworkManager.shared.fetchData() { result in
             switch result {
             case .success(let animals):
                 self.animals = animals
-                self.tableView.reloadData()
             case .failure(let error):
                 print(error)
             }
